f2995a38f627c1de7a411f397d3c6c5cf91f8f8d
DUOLabs333 <dvdugo333@gmail.com>
2023-02-04 18:19:28 -0500

    First version

---
+++ av-forward.py
@@ -0,0 +1,18 @@
+import subprocess
+import argparse,os
+parser = argparse.ArgumentParser(description='Forward MacOS camera/microphone to Linux VM')
+parser.add_argument(dest="mode",metavar="MODE",choices=["client","server"],required=True)
+parser.add_argument("-f","--folder",dest="folder",metavar="FOLDER",default=None)
+
+args = parser.parse_args()
+
+if not args.folder:
+    if args.mode=="client":
+        args.folder="~/Mounts/Macbook"
+    else:
+        args.folder="~/Services/arch/data/shared"
+
+if args.mode=="server":
+    subprocess.run(["sudo","ffmpeg", "-f", "avfoundation", "-framerate 30","-i", "0", "-f", "mpegts","-listen","1",f"unix:{os.path.join(args.folder,'av-forward.camera.socket')}"])
+else:
+    subprocess.run(["sudo","ffmpeg", "-i",f"unix:{os.path.join(args.folder,'av-forward.camera.socket')}", "-vf", "format=yuv420p","-f", "v4l2","/dev/video0"])
