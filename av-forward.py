be7171a273791ecf74ccf74c9cfaa268a668a5e0
DUOLabs333 <dvdugo333@gmail.com>
2023-02-04 18:53:28 -0500

    Fix argument splitting

--- av-forward.py
+++ av-forward.py
@@ -13,6 +13,6 @@ if not args.folder:
         args.folder="~/Services/arch/data/shared"
 
 if args.mode=="server":
-    subprocess.run(["sudo","ffmpeg", "-f", "avfoundation", "-framerate 30","-i", "0", "-f", "mpegts","-listen","1",f"unix:{os.path.join(args.folder,'av-forward.camera.socket')}"])
+    subprocess.run(["sudo","ffmpeg", "-f", "avfoundation", "-framerate","30","-i", "0", "-f", "mpegts","-listen","1",f"unix:{os.path.join(args.folder,'av-forward.camera.socket')}"])
 else:
     subprocess.run(["sudo","ffmpeg", "-i",f"unix:{os.path.join(args.folder,'av-forward.camera.socket')}", "-vf", "format=yuv420p","-f", "v4l2","/dev/video0"])
