3faef276ec064f06cdb1be3078e655570c9f9583
DUOLabs333 <dvdugo333@gmail.com>
2023-08-18 12:35:20 -0400

    Add support for a MultiWrite in server

--- server/server.go
+++ server/server.go
@@ -4,11 +4,13 @@ import (
 	"io"
 	"sync"
 	"fmt"
+	"bufio"
 )
 
 type Server struct{
 	Listener net.Listener
 	Writer *MultiWriter
+	Reader *MultiReader
 }
 func (server *Server) Start(host string, port int){
 	var err error
@@ -18,7 +20,8 @@ func (server *Server) Start(host string, port int){
 		panic(err)
 	}
 	
-	server.Writer=New(io.Discard);
+	server.Writer=NewMW(io.Discard);
+	server.Reader=NewMR(io.NopCloser(bufio.NewReader(nil)));
 	go handleConns(server);
 	return
 }
@@ -37,6 +40,7 @@ func handleConns(server *Server){
 
 func handleIncomingRequest(server *Server,conn net.Conn) {
 	server.Writer.Add(conn)
+	server.Reader.Add(conn) //Maybe check OFF/ON instead of lsof checking ports. Additionally, instead of checking ps -ostat, just go func wait in the background
 }
 
 
@@ -52,7 +56,7 @@ type MultiWriter struct {
 //
 // Each write is written to each listed writer, one at a time. If a listed
 // writer returns an error, remove from the list and continue
-func New(writers ...io.Writer) *MultiWriter {
+func NewMW(writers ...io.Writer) *MultiWriter {
 	mw := &MultiWriter{writers: writers}
 	return mw
 }
@@ -102,6 +106,70 @@ func (mw *MultiWriter) Remove(w io.Writer) {
 func (mw *MultiWriter) Close() error{
 	for _, ew := range mw.writers {
 		mw.Remove(ew)
+	}
+	return nil
+}
+
+
+// MultiReader is a reader that reads from multiple other readers.
+type MultiReader struct {
+	sync.RWMutex
+	readers []io.Reader
+}
+
+
+// Each read is read from each listed reader, one at a time. If a listed
+// reader returns an error, remove from the list and continue
+func NewMR(readers ...io.Reader) *MultiReader {
+	mr := &MultiReader{readers: readers}
+	return mr
+}
+
+// Write reads some bytes from all the readers.
+func (mr *MultiReader) Read(p []byte) (n int, err error) {
+	//fmt.Printf("Num of readers: %d\n",len(mr.readers))
+	mr.RLock()
+	defer mr.RUnlock()
+
+	for _, w := range mr.readers {
+		n, err = w.Read(p)
+		if err != nil {
+			mr.Remove(w)
+			continue
+		}
+
+		if n < len(p) {
+			err = io.ErrUnexpectedEOF
+			return
+		}
+	}
+
+	return len(p), nil
+}
+
+// Add appends a reader to the list of readers this multireader reads to.
+func (mr *MultiReader) Add(w io.Reader) {
+	mr.Lock()
+	mr.readers = append(mr.readers, w)
+	mr.Unlock()
+}
+
+// Remove will remove a previously added reader from the list of readers.
+func (mr *MultiReader) Remove(w io.Reader) {
+	mr.RLock()
+	var readers []io.Reader
+	for _, ew := range mr.readers {
+		if ew != w {
+			readers = append(readers, ew)
+		}
+	}
+	mr.readers = readers
+	mr.RUnlock()
+}
+
+func (mr *MultiReader) Close() error{
+	for _, ew := range mr.readers {
+		mr.Remove(ew)
 	}
 	return nil
 }
