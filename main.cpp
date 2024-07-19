a58ca01c714ee592336234c1059935b3de4be0f5
DUOLabs333 <dvdugo333@gmail.com>
2024-07-19 18:08:31 -0400

    Make some more progress

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <asio_c.h>
 #include <boost/process.h>
+#include <boost/process/io.hpp>
 #include <condition_variable>
 #include <glaze/json/read.hpp>
+#include <regex>
 #include <string_view>
 #include <unordered_map>
 #include <set>
@@ -10,6 +12,11 @@
 #include <glaze/glaze.hpp>
 #include <mutex>
 #include <thread>
+#include <atomic>
+#include <boost/process.hpp>
+#include <regex>
+
+namespace bp = boost::process;
 
 //https://superuser.com/questions/438390/creating-mp4-videos-ready-for-http-streaming
 //https://stackoverflow.com/questions/61990828/how-to-redirect-an-audio-stream-to-a-virtual-pulseaudio-microphone-with-ffmpeg
@@ -20,15 +27,14 @@ typedef struct Device {
 	
 	enum {VIDEO=0, AUDIO=1} type;
 	std::string name;
-	std::pair<int, int> size;
+	std::array<int,2> size;
 	struct glaze {
-		using T = Device;
-		static constexpr auto value = glz::object(&T::type, &T::name, &T::size);
+		static constexpr auto value = glz::object(&Device::type, &Device::name, &Device::size);
 	};
 
 	std::string buf;
 
-	std::any process; //Don't know what type Boost.Process uses
+	bp::child process; //Don't know what type Boost.Process uses
 	bool started = false;
 
 	std::string cmd(){
@@ -43,14 +49,33 @@ typedef struct Device {
 		}
 	}
 
+	void start(){
+		if (!started){
+			process={cmd()};
+			started=true;
+		}
+	}
+
+	void stop(){
+		if(started){
+			process.terminate();
+			process.wait();
+			started=false;
+			
+		}
+	}
+
+	std::mutex mu;
+	std::condition_variable cv;
+
 	#ifndef CLIENT
 		std::set<AsioConn*> conns;
-		std::mutex mu;
-		std::condition_variable cv;
 
 		int number; //Relative to the video/audio list
 
 		std::vector<AsioConn*> closed_conns;
+	#else
+		std::atomic<int> num_procs = 0;
 	#endif
 	
 
@@ -67,9 +92,8 @@ void addConn(int id, AsioConn* conn){ //For server
 	device.mu.lock();
 
 	device.conns.insert(conn);
-	if ((device.conns.size()>=1) && !device.started){
-		device.process=device.cmd().start();
-		device.started=true;
+	if ((device.conns.size()>=1)){
+		device.start();
 	}
 
 	device.mu.unlock();
@@ -85,10 +109,8 @@ void removeConn(int id, AsioConn* conn){
 	device.conns.erase(conn);
 	asio_remove(conn);
 
-	if ((device.conns.size()==0) && device.started){
-		device.process.stop();
-		device.process.wait();
-		device.started=false;
+	if ((device.conns.size()==0)){
+		device.stop();
 	}
 
 	device.mu.unlock();
@@ -129,11 +151,9 @@ void handleConn(AsioConn* conn){ //For servers
 		for(;;){
 			std::unique_lock lk(device.mu);
 			device.cv.wait(lk, [&]{return !device.conns.empty();});
-			lk.unlock();
 			
-			device.process.read(device.buf.data(), device.buf.size()); //I don't know the actual API for Boost::Process
-			
-			lk.lock();
+			device.process.stdout.read(device.buf.data(), device.buf.size()); //I don't know the actual API for Boost::Process
+
 
 			bool err;
 			for(auto& conn : device.conns){
@@ -155,12 +175,50 @@ void handleConn(AsioConn* conn){ //For servers
 }
 #else
 	void handleDevice(int id){
+		//Make device as well
 		auto& device=available_devices[id];
-		uint8_t info[2];
+		uint8_t info[] = {1, id};
+		AsioConn* client =NULL;
+		bool err;
 
+		char* buf;
+		int len;
+		
+		//Call lsof/fuser every 10 s
+		std::thread watch(countOpenHandles, std::ref(device));
 		while(true){
-			auto client
+			asio_close(client);
+			device.stop();
+			
+			std::unique_lock lk(device.mu);
+			device.cv.wait(lk, [&] { return device.num_procs > 0;});
+
+			client=asio_connect(2);
+
+			asio_write(client, (char*)info, sizeof(info), &err);
+
+			if (err){
+				continue;
+			}
+			
+			device.start();
+			while(true){
+				if (device.num_procs==0){
+					break;
+				}
+				asio_write(client, &buf, &len, &err);
+				if (err){
+					break;
+				}
+
+				device.process.stdin.write(buf, len);
+
+			}
+
+
 		}
+
+		w.join();
 	}
 #endif
 
@@ -207,9 +265,44 @@ void connectToServer(){
 
 }
 	
-	
 
+int main(){
 
+	#ifdef CLIENT
+		connectToServer();
+	#else
+		bp::ipstream is;
+		bp::child c (bp::search_path("ffmpeg"),"-f", "avfoundation", "-list_devices", "true", "-i", "\"\"", bp::std_out > bp::null, bp::std_err > is, bp::std_in < bp::null);
+		
+		int mode=-1;
+		
+		int id=0;
+		for (std::string line; std::getline(is, line);){
+			std::string regex_header=R"(\[.+\]\s+)";
+			std::regex device_regex(regex_header+R"#(\[(\d+\)]\s+(.+))#");
+			if (mode==-1){
+				if (std::regex_match(line, std::regex(regex_header+"(AVFoundation video devices))#"))){
+				mode=0;
+		}else if (std::regex_match(line, std::regex(regex_header+"(AVFoundation audio devices))#"))){
+		mode=1;
+	}
+		continue;
+		}else {
+			std::smatch match;
+			auto matched=std::regex_match(line, device_regex);
+			if (!matched){
+				continue;
+			}
+			auto& device=available_devices[id++];
+			device.number=stoi(match.str(1));
+			device.name=match.str(2);
 
+		}
+
+		}
+
+	#endif
+
+}
 
 
